def241fa0d5400a326959f52fbbe7e6b123532b8
Markus Meier <mame-erl@users.noreply.github.com>
2016-05-16 15:38:58 +0200

    Ergänzung Kommentare TSL2591

--- tsl2591_dyna.py
+++ tsl2591_dyna.py
@@ -1,3 +1,10 @@
+# 
+#	TSL2591 Ansteuerung in Python
+#	am Raspberry Pi über I2C
+#	
+#	2016-03-20 Erstellt von Markus Meier
+#
+
 import tsl2591
 
 INTEGRATIONTIME_100MS = 0x00
@@ -19,28 +26,35 @@ tsl.set_timing(INTEGRATIONTIME_200MS)
 none		= tsl.get_full_luminosity()
 full, ir	= tsl.get_full_luminosity()
 
+# Abfangen von ungültigen Infrarot-Messungen bei wenig Licht
 if ir==0:
         tsl.set_gain(GAIN_MAX)
 	tsl.set_timing(INTEGRATIONTIME_500MS)
         full, ir= tsl.get_full_luminosity()
 
+# Abfangen von Überlauf im kombinierten Spektrum
 if full==65535:
 	tsl.set_timing(INTEGRATIONTIME_100MS)
 	tsl.set_gain(GAIN_LOW)
 	full, ir= tsl.get_full_luminosity()
 
+# Abfangen von Überlauf im Infrarot-Spektrum bei viel Licht
 if ir==65535:
 	tsl.set_timing(INTEGRATIONTIME_100MS)
 	tsl.set_gain(GAIN_LOW)
 	full, ir= tsl.get_full_luminosity()
 
-
+# Ermitteln des Lux-Wertes
 lux		= tsl.calculate_lux(full, ir)
 
+# Falls das errechnete Lux-Ergebnis negativ ist, wird
+# die Messung mit höherer Empfindlichkeit und längerer
+# Integrationszeit wiederholt
 if lux<0:
 	tsl.set_gain(GAIN_MAX)
         tsl.set_timing(INTEGRATIONTIME_500MS)
         full, ir= tsl.get_full_luminosity()
 	lux     = tsl.calculate_lux(full, ir)
 
+# Ausgabe des Messergebnisses als Lux-Wert sowie Volles Spektrum und Infrarot
 print ('Lux=%0.2f full=%i ir=%i' % (lux, full, ir))
