c6d293dfd9a235b60f670ef2ae811a3d6a0eff7a
Markus Meier <mame-erl@users.noreply.github.com>
2016-05-17 20:09:25 +0200

    Kommentare für Upload-Skript

--- upload.sh
+++ upload.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-mydate="$(date +%Y-%m-%d:%H-%M-%S)"
+# Created by Markus Meier
 pfad="/home/pi/programme/"
 program_temp="dht22_mm_csv"
 program_lux="tsl2591_dyna.py"
-logpfad="$pfad"
-logdatei="2016.txt"
+
 # API Key for thingspeak is kept separate
 source /home/pi/programme/apikeys.sh
 urlthing="https://api.thingspeak.com/update?api_key=$apithing&"
 
+# Read temperature and humidity from DHT22
 ergebnis="$(sudo $pfad$program_temp)"
+# Separate temperature and humidity into dedicated variables
 wert1="$(echo $ergebnis | cut -d / -f 1)"
 wert2="$(echo $ergebnis | cut -d / -f 2)"
-wert3="$(echo $ergebnis | cut -d / -f 3)"
 
+# Read ambient light from TSL2591
 ergebnis="$(python $pfad$program_lux)"
+# Get only Lux value from TSL2591 output
 wert4="$(echo $ergebnis | cut -d = -f 2 | cut -d " " -f 1)"
 
+# Create URL parameters for ThingSpeak
 feld1="field1=$wert1"
 feld2="&field2=$wert2"
-feld3="&field3=$wert3"
 feld4="&field4=$wert4"
 
-gesamturl="$urlthing$feld1$feld2$feld3$feld4"
+# Create combined URL
+gesamturl="$urlthing$feld1$feld2$feld4"
 
+# Use cURL to HTTP GET the results to ThingSpeak API
 curl "$gesamturl"
-
-echo "$mydate|$wert1:$wert2:$wert3:$wert4" >> $logpfad$logdatei
